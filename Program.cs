9f76ff12eae8894c2404ac6eaf725797579812c2
Vittoria Lopes <Lopes.tt06@gmail.com>
2025-03-23 16:06:29 -0300

    Atividade Concluida

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.AccessControl;
+
+Console.Clear();
+
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+Console.WriteLine("-Sistema de Criptografia em Leet");
+Console.ResetColor();
+
+    Console.WriteLine("Insira a sua fraze e ela será convertida em Leet:");
+
+    string text;
+    string textM;
+    string codigo;
+
+text = Console.ReadLine();
+textM = text.ToUpper();
+codigo = textM.Replace("A", "4")
+                 .Replace("E", "3")
+                 .Replace("O", "0")
+                 .Replace("I", "1");
+
+Console.WriteLine($"Texto em Leet: {codigo}");
+
+
